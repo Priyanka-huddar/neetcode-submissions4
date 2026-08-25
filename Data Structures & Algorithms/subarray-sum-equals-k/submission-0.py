adb38ded5b4562e29af5e1dcc838dc756ff4e42e
Priyanka huddar <priyanka.cd24@bmsce.ac.in>
2026-08-25 10:32:56 +0530

    Add: subarray-sum-equals-k - submission-0

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        freq={0:1}
+        count=0
+        prefix_sum=0
+        for num in nums:
+            prefix_sum+=num
+
+            count+=freq.get(prefix_sum-k,0)
+
+            freq[prefix_sum]=freq.get(prefix_sum,0)+1
+        return count
+        
